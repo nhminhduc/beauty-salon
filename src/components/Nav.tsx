6b0eb3f832981f2ab900737347a7a788a1061694
Duc Nguyen <nguyenhoangminhduc@gmail.com>
2024-10-19 19:04:08 +0300

    Fix

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,38 +1,39 @@
 import Link from "next/link";
-import { usePathname, useRouter } from "next/navigation";
-import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
 import { useTranslations } from "next-intl";
 
 const Nav = () => {
-  const pathName = usePathname();
   const router = useRouter();
   const t = useTranslations("navigation");
-  const [visibleSection, setVisibleSection] = useState("");
+
+  useEffect(() => {
+    const handleSmoothScroll = (e: MouseEvent) => {
+      const target = e.target as HTMLAnchorElement;
+      if (
+        target.hash &&
+        target.origin + target.pathname === window.location.href
+      ) {
+        e.preventDefault();
+        const element = document.querySelector(target.hash);
+        if (element) {
+          element.scrollIntoView({ behavior: "smooth" });
+        }
+      }
+    };
+
+    document.addEventListener("click", handleSmoothScroll);
+    return () => document.removeEventListener("click", handleSmoothScroll);
+  }, []);
 
   const handleLinkClick = (
     e: React.MouseEvent<HTMLAnchorElement>,
     href: string
   ) => {
-    if (href.startsWith("#")) {
-      e.preventDefault();
-      const element = document.querySelector(href);
-      if (element) {
-        element.scrollIntoView({ behavior: "smooth" });
-      }
-    } else {
+    if (!href.startsWith("#")) {
       e.preventDefault();
       router.push(href);
     }
-  };
-
-  const isActive = (href: string) => {
-    if (href === "/") {
-      return pathName === href && !visibleSection;
-    }
-    if (href.startsWith("#")) {
-      return visibleSection === href.slice(1); // Remove the '#' from href
-    }
-    return pathName?.startsWith(href);
   };
 
   return (
@@ -40,21 +41,16 @@ const Nav = () => {
       <div className="container mx-auto flex gap-8">
         {t
           .raw("links")
-          .map((link: { name: string; href: string }, idx: number) => {
-            const active = isActive(link.href);
-            return (
-              <Link
-                key={idx}
-                href={link.href}
-                onClick={(e) => handleLinkClick(e, link.href)}
-                className={`${
-                  active ? "border-b-2 border-accent" : ""
-                } uppercase`}
-              >
-                {link.name}
-              </Link>
-            );
-          })}
+          .map((link: { name: string; href: string }, idx: number) => (
+            <Link
+              key={idx}
+              href={link.href}
+              onClick={(e) => handleLinkClick(e, link.href)}
+              className={`uppercase`}
+            >
+              {link.name}
+            </Link>
+          ))}
       </div>
     </nav>
   );
