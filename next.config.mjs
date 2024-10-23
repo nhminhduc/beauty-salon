4ebd207f0fdfe43adab57e4241f0a60ad8320217
Duc Nguyen <nguyenhoangminhduc@gmail.com>
2024-10-23 17:30:02 +0300

    Deploy on vercel

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,8 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: process.env.NODE_ENV === "production" ? "/beauty-salon" : "",
-  output: "export",
+  // basePath: process.env.NODE_ENV === "production" ? "/beauty-salon" : "",
+  // output: "export",
   reactStrictMode: true,
 };
 
